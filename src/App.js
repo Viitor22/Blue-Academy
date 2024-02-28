4ffed1a0a2f5d69726ab93b1541a0e368cd168cc
Viitor22 <scizor987@gmail.com>
2024-02-28 14:05:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import EstiloGlobal, {Container, Titulo, Texto, Secao, Botao} from "./Style";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Secao>
+          <Titulo>
+            A sua aprovação em medicina
+            é importante demais para você gastar o seu tempo estudando
+            com métodos que não vão te
+            ajudar a passar.
+          </Titulo>
+          <Botao>Quero passar em Medicina no ENEM</Botao>
+        </Secao>
+        <Titulo></Titulo>
+        <Texto>
+          Ao contrário das escolas e cursinhos tradicionais - que te ensinam
+          sobre as matérias, mas não te mostram como estudar de maneira
+          estratégica para passar no Enem - nós montamos a sua estratégia
+          de estudos para que você consiga ser aprovado em medicina
+          em apenas um ano.
+        </Texto>
+      </Container>
+    </>
   );
 }
 
